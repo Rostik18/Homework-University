06edb2e06f370ea32bc494c8ac4faf638afcff8f
Rostik <rostik1804@gmail.com>
2019-09-14 16:38:13 +0300

    BA-6 Add DbContextA and migrate it

---
+++ BankApplication/BankApplication/BankDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankApplication {
+
+    class BankDbContext : DbContext {
+
+        public BankDbContext() {
+
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder ) {
+
+            optionsBuilder.UseSqlServer( "Server=(localdb)\\mssqllocaldb;Database=BankDB;Trusted_Connection=True;" );
+        }
+    }
+}
