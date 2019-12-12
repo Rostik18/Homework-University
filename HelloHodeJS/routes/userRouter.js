006a3105d097b690af976d49f4f7147c44d6fe86
Rostik <rostik1804@gmail.com>
2019-12-12 11:18:22 +0200

    Add my training project

---
+++ HelloHodeJS/routes/userRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const userController = require("../controllers/userController.js");
+const userRouter = express.Router();
+ 
+userRouter.use("/postuser", userController.postUser);
+userRouter.use("/create", userController.addUser);
+userRouter.use("/update/:id", userController.updateUser);
+userRouter.use("/putuser", userController.putUser);
+userRouter.use("/delete", userController.deleteUser);
+userRouter.use("/", userController.getUsers);
+ 
+module.exports = userRouter;
