533fd0281003c6234db2b2afcebfe44e58c7dd12
Rostik <rostik1804@gmail.com>
2020-04-02 14:15:50 +0300

    Task 4 NonlinearNewtonMethod

---
+++ NumericalMethods/NonlinearNewtonMethod/NonlinearNewtonMethod/GaussProgram.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonlinearNewtonMethod
+{
+    class GaussProgram
+    {
+        public int n;
+        public double[,] A;
+        public double[] b;
+        public double[] x;
+        public double det_A;
+        private static int swapCount;
+
+        public GaussProgram()
+        {
+            swapCount = 0;
+            det_A = 1;
+        }
+
+        private void SwapLines(int k)
+        {
+            int goodLine = k;
+            double max = A[k, k];
+            for (int i = k; i < n; i++)
+            {
+                if (A[i, i] != 0 && i != k && Math.Abs(A[i, i]) > Math.Abs(max))
+                {
+                    max = A[i, i];
+                    goodLine = i;
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                double swap_tmp = A[k, j];
+                A[k, j] = A[goodLine, j];
+                A[goodLine, j] = swap_tmp;
+            }
+            double tmp_b = b[k];
+            b[k] = b[goodLine];
+            b[goodLine] = tmp_b;
+            swapCount++;
+
+        }
+
+        public void Method()
+        {
+            x = new double[n];
+
+            for (int k = 0; k < n - 1; k++)
+            {
+                if (A[k, k] == 0) SwapLines(k);
+
+                for (int i = k + 1; i < n; i++)
+                {
+                    double m = -A[i, k] / A[k, k];
+                    b[i] = b[i] + m * b[k];
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        A[i, j] = A[i, j] + m * A[k, j];
+                    }
+                    A[i, k] = 0;
+                }
+            }
+            x[n - 1] = b[n - 1] / A[n - 1, n - 1];
+            for (int k = n - 2; k >= 0; k--)
+            {
+                x[k] = (b[k] - sum(k, x)) / A[k, k];
+            }
+            //det_A = 1;
+            for (int i = 0; i < n; i++)
+            {
+                det_A *= A[i, i];
+            }
+            if (swapCount % 2 == 1)
+                det_A *= -1;
+        }
+
+        private double sum(int k, double[] x)
+        {
+            double rez = 0;
+            for (int j = k + 1; j < n; j++)
+            {
+                rez += A[k, j] * x[j];
+            }
+            return rez;
+        }
+
+        public double[] Run(int n, double[,] A, double[] b)
+        {
+            this.n = n;
+            this.A = (double[,])A.Clone();
+            this.b = (double[])b.Clone();
+
+            Method();
+
+            return x;
+        }
+    }
+}
---
+++ NumericalMethods/NonlinearNewtonMethod/NonlinearNewtonMethod/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace NonlinearNewtonMethod
+{
+    // Метод Ньютона для розв'язування нелінійних систем з ДВОМА рівняннями.
+    class NonlinearNewtonProgram
+    {
+        const double e = 0.000001;
+        GaussProgram gauss = new GaussProgram();
+
+        double[] F(double x, double y)
+        {
+            return new double[]
+            {
+                Math.Sin(x+1) - y - 1,
+                2*x + Math.Cos(y) - 2
+            };
+        }
+
+        double[,] JF(double x, double y)
+        {
+            return new double[,]
+            {
+                {
+                    Math.Cos(x + 1), -1
+                },
+                {
+                    2, -Math.Sin(y)
+                }
+            };
+        }
+
+        public double[] Run(double x0, double y0)
+        {
+            int iter = 0;
+
+            double x;
+            double y;
+
+            while (true)
+            {
+                var jF = JF(x0, y0);
+                var f = F(x0, y0);
+
+                Console.WriteLine($"{iter++}: f({x0:f8} ; {y0:f8}) = ({f[0]:f5} ; {f[1]:f5})");
+
+                var dxy = gauss.Run(2, jF, new double[] { -f[0], -f[1] });
+
+                x = x0 + dxy[0];
+                y = y0 + dxy[1];
+
+                if (Math.Max(Math.Abs(-x + x0), Math.Abs(-y + y0)) < e) break;
+
+                x0 = x;
+                y0 = y;
+            }
+
+            return new double[] { x, y };
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NonlinearNewtonProgram program = new NonlinearNewtonProgram();
+
+            program.Run(1, 1);
+        }
+    }
+}
