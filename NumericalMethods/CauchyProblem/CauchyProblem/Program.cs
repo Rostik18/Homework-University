5bd744cbfbb6eee3978eeb6ca0a17b949cd1febe
Rostik <rostik1804@gmail.com>
2020-04-18 11:55:13 +0300

    Task - 4 Add Runge - Kutta methods

--- NumericalMethods/CauchyProblem/CauchyProblem/Program.cs
+++ NumericalMethods/CauchyProblem/CauchyProblem/Program.cs
@@ -51,6 +51,26 @@ namespace CauchyProblem
 
             Console.WriteLine($"Heine method\ny = {y}");
         }
+
+        public void RungeKuttaMethod(double x, double y, double h)
+        {
+            double exectY = x + 2 * Math.Atan(1 / x);
+            Console.WriteLine($"Exact solution y* = {exectY} for x = {x}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                x = x + (i + 1) * h;
+
+                var k1 = F(x, y);
+                var k2 = F(x + (h / 2), y + (h / 2) * k1);
+                var k3 = F(x + (h / 2), y + (h / 2) * k2);
+                var k4 = F(x + h, y + h * k3);
+
+                y = y + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+            }
+
+            Console.WriteLine($"Runge - Kutta method\ny = {y}");
+        }
     }
 
     // Розв'язування систем задач Коші.
@@ -100,6 +120,31 @@ namespace CauchyProblem
 
             Console.WriteLine($"System Heine method\ny = {y}\nz = {z}");
         }
+
+        public void RungeKuttaMethod(double x, double y, double z, double h)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                x = x + (i + 1) * h;
+
+                var k1 = Fg(x, y, z)[0];
+                var l1 = Fg(x, y, z)[1];
+
+                var k2 = Fg(x + (h / 2), y + (h / 2) * k1, z + (h / 2) * l1)[0];
+                var l2 = Fg(x + (h / 2), y + (h / 2) * k1, z + (h / 2) * l1)[1];
+
+                var k3 = Fg(x + (h / 2), y + (h / 2) * k2, z + (h / 2) * l2)[0];
+                var l3 = Fg(x + (h / 2), y + (h / 2) * k2, z + (h / 2) * l2)[1];
+
+                var k4 = Fg(x + h, y + h * k3, z + h * l3)[0];
+                var l4 = Fg(x + h, y + h * k3, z + h * l3)[1];
+
+                y = y + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+                z = z + (h / 6) * (l1 + 2 * l2 + 2 * l3 + l4);
+            }
+
+            Console.WriteLine($"System Runge - Kutta method\ny = {y}\nz = {z}");
+        }
     }
 
     class Program
@@ -110,11 +155,13 @@ namespace CauchyProblem
 
             cauchyProgram.EulerMethod(1, 2.57, 0.1);
             cauchyProgram.HeineMethod(1, 2.57, 0.1);
+            cauchyProgram.RungeKuttaMethod(1, 2.57, 0.1);
 
             CauchySystemProgram cauchySystemProgram = new CauchySystemProgram();
 
             cauchySystemProgram.EulerMethod(0, 0, 4, 0.01);
             cauchySystemProgram.HeineMethod(0, 0, 4, 0.01);
+            cauchySystemProgram.RungeKuttaMethod(0, 0, 4, 0.01);
         }
     }
 }
