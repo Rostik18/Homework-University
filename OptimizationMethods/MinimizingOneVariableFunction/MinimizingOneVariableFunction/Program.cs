bcd2767419fc5fa6946630c90b0b70e35c64ac01
Rostik <rostik1804@gmail.com>
2020-04-27 19:27:34 +0300

    Task - 1 DCK and DichotomyMethod fixes

--- OptimizationMethods/MinimizingOneVariableFunction/MinimizingOneVariableFunction/Program.cs
+++ OptimizationMethods/MinimizingOneVariableFunction/MinimizingOneVariableFunction/Program.cs
@@ -34,19 +34,23 @@ namespace MinimizingOneVariableFunction
 
             Console.WriteLine($"Iteration {dichotomyIterationCount:d2}: [{a:f3} - {b:f3}]");
 
-            double c = (a + b) / 2;
+            var siq = e / 2;
+            var c = (a + b) / 2;
 
-            if ((b - a) < e)
+            var x1 = (a + b - siq) / 2;
+            var x2 = (a + b + siq) / 2;
+
+            if ((Math.Abs(b - a)) < e)
             {
                 return c;
             }
-            else if (F(c - e) < F(c + e))
+            else if (F(x1) < F(x2))
             {
-                return DichotomyMethod(a, c);
+                return DichotomyMethod(a, x2);
             }
             else
             {
-                return DichotomyMethod(c, b);
+                return DichotomyMethod(x1, b);
             }
         }
 
@@ -56,10 +60,8 @@ namespace MinimizingOneVariableFunction
 
             int n = fibNumbs.Count - 1;
 
-            double x1, x2;
-
-            x1 = a + (b - a) * (fibNumbs[n - 2] / fibNumbs[n]);
-            x2 = a + (b - a) * (fibNumbs[n - 1] / fibNumbs[n]);
+            var x1 = a + (b - a) * (fibNumbs[n - 2] / fibNumbs[n]);
+            var x2 = a + (b - a) * (fibNumbs[n - 1] / fibNumbs[n]);
 
             double f1 = F(x1);
             double f2 = F(x2);
@@ -97,7 +99,7 @@ namespace MinimizingOneVariableFunction
             if (F(x) < F(x + h))
             {
                 h = -h;
-            }
+            };
 
             while (F(x) >= F(x + h))
             {
@@ -105,8 +107,21 @@ namespace MinimizingOneVariableFunction
                 h = 2.0 * h;
             }
 
-            a = x - h;
-            b = (x + h) - (h / 2.0);
+            var xm1 = x + h;
+            var xm2 = x;
+            var xm3 = x - h / 2;
+            var xm4 = x + h / 2;
+
+            if (F(xm3) > F(xm1))
+            {
+                a = xm2;
+                b = xm1;
+            }
+            else
+            {
+                a = xm3;
+                b = xm4;
+            }
         }
 
         static void Main(string[] args)
