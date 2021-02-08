227d8bf57a748527a70a03c566445536024d2f58
Rostik <38851562+Rostik18@users.noreply.github.com>
2021-02-08 16:30:23 +0200

    Merge Data analysis tools
    * Add model
    
    * Complete HeartDiseaseСlassification
    
    * Add IrisClustering

---
+++ DataAnalysisTools/IrisClustering/IrisClustering/Data/ClusterPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace IrisClustering.Data
+{
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
