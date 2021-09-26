f134900183b437fd27b8fee5d9061804576bfce7
RostyslavBaitsar <rostik1804@gmail.com>
2021-09-26 14:38:53 +0300

    DMIC-2: Method of possible directions stochastic analogue WIP

---
+++ DecisionMakingInConflict/Lab2_MethodOfPossibleDirectionsStochasticAnalogue/MethodOfPossibleDirectionsStochasticAnalogue/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MethodOfPossibleDirectionsStochasticAnalogue
+{
+    public partial class Form1 : Form
+    {
+        private const int n = 2;
+
+        private List<Function2To1> _availableFunctions = new List<Function2To1>
+        {
+            new Function2To1{Label = "x^2 + y^2", F = (double x, double y) => x*x + y*y }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            availableFunctionsComboBox.DataSource = _availableFunctions;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var alfa = 1;
+            var ro0 = 1 / (n + 1.0);
+
+            var x = (0, 0);
+
+            for (int k = 0; k < 100; k++)
+            {
+                //  var eps = ;
+
+                //  ro 
+
+                //step 6
+                //  x = x + ro * h;
+            }
+
+        }
+
+        private Func<double, double, double> GetCurrentFunction() => ((Function2To1)availableFunctionsComboBox.SelectedItem).F;
+    }
+}
---
+++ DecisionMakingInConflict/Lab2_MethodOfPossibleDirectionsStochasticAnalogue/MethodOfPossibleDirectionsStochasticAnalogue/Function2To1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodOfPossibleDirectionsStochasticAnalogue
+{
+    public class Function2To1
+    {
+        public string Label { get; set; }
+        public Func<double, double, double> F { get; set; }
+
+        public override string ToString() => Label;
+    }
+}
