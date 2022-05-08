1757f2b90f2ca4d6b58048c7d317218bb612b076
RostyslavBaitsar <rostik1804@gmail.com>
2022-05-08 15:46:22 +0300

    MMDMDC: lab-2 NFA, FFA, WFA, BFA

---
+++ MathMethodsOfDecisionMakingInDefinedCondition/ContainerPackingProblem/ContainerPackingProblem/AlgorithmsWithoutOrdering.cs
@@ -0,0 +1,133 @@
+﻿
+namespace ContainerPackingProblem
+{
+    static class AlgorithmsWithoutOrdering
+    {
+        const int ContainerCapacity = 100;
+
+        public static int MinContainersNeed(IEnumerable<int> elements) => elements.Sum() / ContainerCapacity;
+
+        // Next Fit Algorithm
+        public static List<int[]> NFA(IEnumerable<int> elements)
+        {
+            var containers = new List<int[]>();
+            var container = new List<int>();
+
+            foreach (var element in elements)
+            {
+                if (container.Sum() + element > ContainerCapacity)
+                {
+                    containers.Add(container.ToArray());
+                    container = new List<int>();
+                }
+
+                container.Add(element);
+            }
+
+            return containers;
+        }
+
+        // First Fit Algorithm
+        public static List<List<int>> FFA(IEnumerable<int> elements)
+        {
+            var containers = new List<List<int>>();
+
+            foreach (var element in elements)
+            {
+                var i = containers.Count - 1;
+                for (; i >= 0; i--)
+                {
+                    if (containers[i].Sum() + element <= ContainerCapacity)
+                    {
+                        containers[i].Add(element);
+                        break;
+                    }
+                }
+                if (i == -1)
+                {
+                    containers.Add(new List<int> { element });
+                }
+            }
+
+            return containers;
+        }
+
+        // Worst Fit Algorithm
+        public static List<List<int>> WFA(IEnumerable<int> elements)
+        {
+            var containers = new List<List<int>> { new List<int>() };
+
+            foreach (var element in elements)
+            {
+                var mostFreeContainer = containers.Last();
+
+                if (mostFreeContainer.Sum() + element <= ContainerCapacity)
+                {
+                    mostFreeContainer.Add(element);
+                }
+                else
+                {
+                    for (var i = containers.Count - 1; i >= 0; i--)
+                    {
+                        if (containers[i].Sum() < mostFreeContainer.Sum())
+                        {
+                            mostFreeContainer = containers[i];
+                        }
+                    }
+
+                    if (mostFreeContainer.Sum() + element <= ContainerCapacity)
+                    {
+                        mostFreeContainer.Add(element);
+                    }
+                    else
+                    {
+                        mostFreeContainer = new List<int> { element };
+                        containers.Add(mostFreeContainer);
+                    }
+                }
+            }
+
+            return containers;
+        }
+
+        // Best Fit Algorithm
+        public static List<List<int>> BFA(IEnumerable<int> elements)
+        {
+            var containers = new List<List<int>> { new List<int>() };
+
+            foreach (var element in elements)
+            {
+                var bestContainer = containers.Last();
+
+                if (bestContainer.Sum() + element <= ContainerCapacity)
+                {
+                    bestContainer.Add(element);
+                }
+                else
+                {
+                    var minFreeCapacity = 0;
+                    for (var i = containers.Count - 1; i >= 0; i--)
+                    {
+                        if (containers[i].Sum() > minFreeCapacity && containers[i].Sum() + element <= ContainerCapacity)
+                        {
+                            minFreeCapacity = containers[i].Sum();
+                            bestContainer = containers[i];
+                        }
+                    }
+
+                    if (bestContainer.Sum() + element <= ContainerCapacity)
+                    {
+                        bestContainer.Add(element);
+                    }
+                    else
+                    {
+                        bestContainer = new List<int> { element };
+                        containers.Add(bestContainer);
+                    }
+                }
+            }
+
+            return containers;
+        }
+    }
+}
---
+++ MathMethodsOfDecisionMakingInDefinedCondition/ContainerPackingProblem/ContainerPackingProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Running;
+using ContainerPackingProblem;
+
+var summary = BenchmarkRunner.Run<AlgorithmsWithoutOrderingBenchmarks>();
+
+//var elements = new int[]
+//{
+//    53,66,97,48,47,72,16,86,12,59,77,51,53,97,32,03,35,51,07,98,
+//    49,54,61,06,34,03,25,84,28,97,63,33,15,60,81,14,85,97,51,97,
+//    08,29,49,13,79,34,16,14,85,75,65,86,30,26,92,16,29,69,52,09
+//};
+
+//var minContainers = AlgorithmsWithoutOrdering.MinContainersNeed(elements); // 29
+
+//var first = AlgorithmsWithoutOrdering.NFA(elements);
+//var second = AlgorithmsWithoutOrdering.FFA(elements);
+//var third = AlgorithmsWithoutOrdering.WFA(elements);
+//var fourth = AlgorithmsWithoutOrdering.BFA(elements);
+
+//Console.WriteLine();
+
+
+//|          Method |      Mean |     Error |    StdDev | Allocated |
+//| --------------- | -------- :| ---------:| ---------:| ---------:|
+//| NFA_100elements | 3.042 us  | 0.0584 us | 0.0780 us | 8 KB      |
+//| FFA_100elements | 19.215 us | 0.1473 us | 0.1306 us | 28 KB     |
+//| WFA_100elements | 52.241 us | 0.8813 us | 0.8244 us | 75 KB     |
+//| BFA_100elements | 48.619 us | 0.3212 us | 0.2682 us | 70 KB     |
