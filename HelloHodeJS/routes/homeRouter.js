7d317a11757f0a410b94638c05eac2ca189a752d
LEVI9\r.baitsar <r.baitsar@levi9.com>
2023-09-19 14:58:26 +0300

    remove some alerting trash

--- HelloHodeJS/routes/homeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const homeController = require("../controllers/homeController.js");
-const homeRouter = express.Router();
- 
-homeRouter.get("/about", homeController.about);
-homeRouter.get("/home", homeController.index);
-homeRouter.get("/", homeController.index);
- 
-module.exports = homeRouter;
